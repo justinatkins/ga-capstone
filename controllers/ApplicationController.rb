be94f26acfa1e0138e2a50a732bf4ad20efa7ae0
Justin Atkins <justinatkins@Justins-MacBook-Pro-2.local>
2019-06-03 12:54:15 -0500

    created ApplicationController.rb

---
+++ controllers/ApplicationController.rb
@@ -0,0 +1,34 @@
+class ApplicationController < Sinatra::Base
+	require 'bundler'
+	Bundler.require()
+
+	#environment config
+	require './config/environments'
+
+	enable :sessions
+
+	ActiveRecord::Base.establish_connection(
+		:adapter => 'postgresql',
+		:database => 'guitars'
+	)
+
+	use Rack::MethodOverride
+	set :method_override, true
+
+	set :views, File.expand_path('../../views', __FILE__)
+	set :public_dir, File.expand_path('../../public', __FILE__)
+
+	get '/' do
+		redirect '/recipes'
+	end
+
+	get '/test' do
+		test_text = "Guitarmy is live"
+		binding.pry
+		"Pry finished. #{test_text}"
+	end
+
+	get '*' do
+		halt 404
+	end
+end
